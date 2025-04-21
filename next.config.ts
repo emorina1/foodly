d2287cf45db7db1b208ba29ca4625dc02d565fc0
emorina1 <151447538+emorina1@users.noreply.github.com>
2025-04-21 20:32:19 +0200

    Database Connection
    lidhja me databaz i pranoni request per me pas akses

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  env:{
+    MONGODB_URI:
+    "mongorestore --uri mongodb+srv://elsamorina:RB3cVclRTGvMRkhu@foodly.zeg3wsc.mongodb.net "
+  }
 };
 
 export default nextConfig;
